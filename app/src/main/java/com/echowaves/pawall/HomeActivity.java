0cbe9eb46ade2e6e761ceaa8264a64229ee3e6c7
dmitryame <dmitry@custommode.com>
2015-02-25 16:57:42 -0500

    test

--- app/src/main/java/com/echowaves/pawall/HomeActivity.java
+++ app/src/main/java/com/echowaves/pawall/HomeActivity.java
@@ -11,7 +11,6 @@ public class HomeActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
         // this is a test
     }
 
