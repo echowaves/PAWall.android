2a7ffbf4885877c60fa127d2504d3f567b50007c
dmitryame <dmitry@custommode.com>
2015-03-02 16:01:57 -0500

    Create Post WIP

--- app/src/main/java/com/echowaves/pawall/CreatePostActivity.java
+++ app/src/main/java/com/echowaves/pawall/CreatePostActivity.java
@@ -1,7 +1,10 @@
 package com.echowaves.pawall;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 import com.echowaves.pawall.core.PAWActivity;
@@ -10,6 +13,9 @@ import com.echowaves.pawall.core.PAWActivity;
 public class CreatePostActivity extends PAWActivity {
 
     private ImageButton backButton;
+
+    private EditText bodyText;
+    private ImageButton createPostButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +30,19 @@ public class CreatePostActivity extends PAWActivity {
             }
         });
 
+        createPostButton = (ImageButton) findViewById(R.id.createPost_createPostButton);
+        //Listening to button event
+        createPostButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                finish();
+            }
+        });
+
+        bodyText = (EditText) findViewById(R.id.createPost_bodyText);
+        Typeface font = Typeface.createFromAsset(getAssets(), "fonts/AmericanTypewriter.ttc");
+        bodyText.setTypeface(font);
+
+        bodyText.setTextSize(TypedValue.COMPLEX_UNIT_SP, 17.f);
     }
 
 }
