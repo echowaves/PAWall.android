75428d04d6b707214703c4a7fca50ffcc9cfe275
dmitryame <dmitry@custommode.com>
2015-03-02 20:58:33 -0500

    Create Post WIP

--- app/src/main/java/com/echowaves/pawall/CreatePostActivity.java
+++ app/src/main/java/com/echowaves/pawall/CreatePostActivity.java
@@ -1,19 +1,26 @@
 package com.echowaves.pawall;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.util.TypedValue;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.echowaves.pawall.core.PAWActivity;
+import com.parse.LocationCallback;
+import com.parse.ParseException;
+import com.parse.ParseGeoPoint;
 
 
 public class CreatePostActivity extends PAWActivity {
 
+    protected ParseGeoPoint currentLocation;
     private ImageButton backButton;
-
+    private TextView locationLable;
     private EditText bodyText;
     private ImageButton createPostButton;
 
@@ -30,19 +37,58 @@ public class CreatePostActivity extends PAWActivity {
             }
         });
 
+        bodyText = (EditText) findViewById(R.id.createPost_bodyText);
+        Typeface font = Typeface.createFromAsset(getAssets(), "fonts/AmericanTypewriter.ttc");
+        bodyText.setTypeface(font);
+        bodyText.setEnabled(false);
+
+        locationLable = (TextView) findViewById(R.id.createPost_locationLable);
+
+        ParseGeoPoint.getCurrentLocationInBackground(10000, new LocationCallback() {
+            @Override
+            public void done(ParseGeoPoint geoPoint, ParseException e) {
+                if (e == null) {
+                    // We were able to get the location
+                    currentLocation = geoPoint;
+                    locationLable.setText("Current Location Detected");
+                    bodyText.setEnabled(true);
+                } else {
+                    // handle your error
+                    locationLable.setText("Unable to detect current location. Make sure to enable GPS.");
+                    locationLable.setBackgroundColor(Color.RED);
+                    bodyText.setEnabled(false);
+                }
+            }
+        });
+
         createPostButton = (ImageButton) findViewById(R.id.createPost_createPostButton);
         //Listening to button event
         createPostButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View arg0) {
+                String text = bodyText.getText().toString();
+                if (text.length() < 10) {
+
+                    AlertDialog.Builder alertDialogConfirmWaveDeletion = new AlertDialog.Builder(CreatePostActivity.this);
+                    alertDialogConfirmWaveDeletion.setTitle("Warning");
+
+                    // set dialog message
+                    alertDialogConfirmWaveDeletion
+                            .setMessage("Your post can't be empty. Try again.")
+                            .setCancelable(false)
+                            .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                }
+                            });
+                    // create alert dialog
+                    AlertDialog alertDialog = alertDialogConfirmWaveDeletion.create();
+                    // show it
+                    alertDialog.show();
+                    return;
+                }
                 finish();
             }
         });
 
-        bodyText = (EditText) findViewById(R.id.createPost_bodyText);
-        Typeface font = Typeface.createFromAsset(getAssets(), "fonts/AmericanTypewriter.ttc");
-        bodyText.setTypeface(font);
-
-        bodyText.setTextSize(TypedValue.COMPLEX_UNIT_SP, 17.f);
     }
 
 }
